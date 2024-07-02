98569dc39dbf52a6e98a21996422025cf16f5ac7
Robert Grou <r_grou@yahoo.com>
2024-07-01 20:51:16 -0400

    added comment to V1.0

--- VLC_scheduler.py
+++ VLC_scheduler.py
@@ -1,3 +1,16 @@
+"""
+If you get the following warning when executing:
+main libvlc error: stale plugins cache: modified C:\Program Files\VideoLAN\VLC\plugins\
+
+try doing the following:
+- open a prompt, terminal or cmd window, as administrator
+- navigate to where VLC is installed and look for a program called vlc-cache-gen.exe
+- run or execute the program vlc-cache-gen.exe and pass it the folder where the plugins are stored.
+So in Windows, with terminal as admin navigate to VLC and execute the following:
+vlc-cache-gen.exe \plugins
+this should clear up the warning messages.
+"""
+
 # VLC_scheduler.py
 import vlc  # Ensure you have installed this package using `pip install python-vlc`
 import configparser
